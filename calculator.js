a1f9adeb82986bd11cc1e1d41be9963c298f4170
suganjaneiro <71336308+suganjaneiro@users.noreply.github.com>
2020-10-08 17:14:54 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,6 @@
+var age=prompt("How old are you?");
+var days=age*365.25;
+alert(age +" years is roughly "+days +"days");
+
+//Leap Year has 366 days. So approximately multiply by 365.25 (Quarter of the year)
+//Normal years calculation ,multiply by 365.
